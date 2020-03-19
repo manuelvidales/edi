8044eebeb42a7bd78a87ad74047c5ae22c402b85
manuelvidales <virmanuel@gmail.com>
2020-03-19 17:19:15 -0500

    platilla en markdown para correo daimler

---
+++ resources/views/mails/daimler.blade.php
@@ -0,0 +1,21 @@
+@component('mail::message')
+# Tienes una nueva orden de DAIMLER
+
+Identificacion de envio: **{{ $id}}**<br>
+Fecha de carga: **{{ $fecha}}**
+
+
+| Origen |  | Destino |
+| ------ | ------ | ------ |
+| {{ $origen }} | - | {{ $destino }} |
+
+<br>
+<br>
+Favor de enviar su confirmacion!
+@component('mail::button', ['url' => 'http://192.168.1.220:8012/edidaimler/'.$id, 'color' => 'success']) Ver Orden
+@endcomponent
+
+
+Gracias,<br>
+{{ config('app.name') }}
+@endcomponent
