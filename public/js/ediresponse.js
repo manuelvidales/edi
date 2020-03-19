12ab1a3b124b861462e6a60415a4637fe783bfec
manuelvidales <virmanuel@gmail.com>
2020-03-19 17:16:10 -0500

    enviar form post para EdidaimlerController@respuesta

---
+++ public/js/ediresponse.js
@@ -0,0 +1,63 @@
+/*
+ * MVidales: EDI respuesta para 990.
+ */
+$.ajaxSetup({
+    headers: {
+        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+    }
+});
+$('#aceptarform').submit(function(e){
+    e.preventDefault();
+    
+    let input1 = $('#aceptarform input[name="orderid"]');
+    let input2 = $('#aceptarform input[name="res"]');
+
+    let formData = {
+        orderid: $(input1).val(),
+        response: $(input2).val(),
+    }
+    $.ajax({
+        type: 'POST',
+        url: '/edidaimler/respuesta',
+        data: formData,
+        success: function(data){
+          console.log(data);
+          $('#confirmOrder').modal('show');
+        },
+        error: function(error){
+            console.log(error);
+        }
+    });
+    
+});
+
+$('#denyform').submit(function(e){
+    e.preventDefault();
+    
+    let input1 = $('#denyform input[name="order"]');
+    let input2 = $('#denyform input[name="deny"]');
+
+    let formData = {
+        order: $(input1).val(),
+        deny: $(input2).val(),
+    }
+    $.ajax({
+        type: 'POST',
+        url: '/edidaimler/respuesta',
+        data: formData,
+        success: function(data){
+          console.log(data);
+          $('#confirmOrder').modal('show');
+        },
+        error: function(error){
+            console.log(error);
+        }    
+    });
+    
+});
+
+//Cierra la pagina al cerrar el modal
+$("#confirmOrder").on('hidden.bs.modal', function () {
+    //window.close();
+    window.location = "/edidaimler/alert"
+});
