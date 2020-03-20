1ba1a281eeb3a7a18c97381e8541aaff5c9a80a8
manuelvidales <virmanuel@gmail.com>
2020-03-20 11:41:30 -0500

    cambio home & agrega welcome

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    return view('welcome');
+});
+Route::get('/home', function () {
     return view('home');
 });
 //Preparar array TxT
