fc34ff8edacd2711c1db7adc633b94fbaa631a08
manuelvidales <virmanuel@gmail.com>
2020-04-13 15:40:46 -0500

    test txt

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends ConsoleKernel
      */
     protected $commands = [
         Commands\EdiDaimler::class,
-        Commands\Edi214Daimler::class,
+        //Commands\Edi214Daimler::class,
     ];
 
     /**
