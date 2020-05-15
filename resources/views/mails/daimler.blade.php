c3234b43cfc4140b0157f16de2723661ef5fcec2
manuelvidales <virmanuel@gmail.com>
2020-05-15 14:55:17 -0500

    agrega validacion por code

--- resources/views/mails/daimler.blade.php
+++ resources/views/mails/daimler.blade.php
@@ -1,8 +1,10 @@
 @component('mail::message')
+@if ($code == 0)
 # Tienes una nueva orden de DAIMLER
 
 Identificacion de envio: **{{ $id}}**<br>
-Fecha de carga: **{{ $fecha}}**
+Fecha de carga: **{{ $fecha}}**<br>
+Hora de carga: **{{ $hora}}**
 
 
 | Origen |  | Destino |
@@ -15,6 +17,33 @@ Fecha de carga: **{{ $fecha}}**
 @component('mail::button', ['url' => 'http://192.168.1.220:8012/edidaimler/'.$id, 'color' => 'success']) Ver Orden
 @endcomponent
 
+@elseif($code == 5)
+# DAIMLER Actualizo la orden siguiente:
+
+Identificacion de envio: **{{ $id}}**<br>
+Fecha de carga: **{{ $fecha}}**<br>
+Hora de carga: **{{ $hora}}**
+
+
+| Origen |  | Destino |
+| ------ | ------ | ------ |
+| {{ $origen }} | - | {{ $destino }} |
+
+<br>
+@else
+# Cancelacion de orden por DAIMLER:
+
+Identificacion de envio: **{{ $id}}**<br>
+Fecha de carga: **{{ $fecha}}**<br>
+Hora de carga: **{{ $hora}}**
+
+
+| Origen |  | Destino |
+| ------ | ------ | ------ |
+| {{ $origen }} | - | {{ $destino }} |
+
+<br>
+@endif
 
 Gracias,<br>
 {{ config('app.name') }}
