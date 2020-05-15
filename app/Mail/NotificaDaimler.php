c07d021ac221402463e841c415cca1c49b9cf246
manuelvidales <virmanuel@gmail.com>
2020-05-15 14:54:40 -0500

    agrega variables hora & code

--- app/Mail/NotificaDaimler.php
+++ app/Mail/NotificaDaimler.php
@@ -12,22 +12,26 @@ class NotificaDaimler extends Mailable
     use Queueable, SerializesModels;
 
     public $subject = 'Notificacion Daimler';
+    public $code;
     public $id;
     public $origen;
     public $destino;
     public $fecha;
+    public $hora;
 
     /**
      * Create a new message instance.
      *
      * @return void
      */
-    public function __construct($id, $origen, $destino, $fecha)
+    public function __construct($code, $id, $origen, $destino, $fecha, $hora)
     {
+        $this->code = $code;
         $this->id = $id;
         $this->origen = $origen;
         $this->destino = $destino;
         $this->fecha = $fecha;
+        $this->hora = $hora;
     }
 
     /**
