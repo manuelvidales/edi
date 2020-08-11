8ad0baa25d0d3cb47c1b130be05c99d491a7b055
manuelvidales <virmanuel@gmail.com>
2020-08-11 10:34:02 -0500

    cambio repuesta & index

--- app/Http/Controllers/EdidaimlerController.php
+++ app/Http/Controllers/EdidaimlerController.php
@@ -6,13 +6,37 @@ use App\edidaimler;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
 
 class EdidaimlerController extends Controller
 {
     public function index()
     {
-        $ships = edidaimler::all();
-        return \view('daimler.index', compact('ships'));
+        $ftp_server = env('FTP_HOST');
+        $ftp_user = env('FTP_USERNAME');
+        $ftp_pass = env('FTP_PASSWORD');
+        // establecer una conexión o finalizarla
+        $conn_id = ftp_connect($ftp_server) or die("No se pudo conectar a $ftp_server");
+        $login = ftp_login($conn_id, $ftp_user, $ftp_pass);
+        //validar conexion FTP
+
+        
+
+        if ( @$login ) {
+        // Obtener los archivos del directorio ftp
+            $filesnew = ftp_nlist($conn_id, 'fromRyder');
+            $filesall = ftp_nlist($conn_id, 'fromRyder_arch');
+            $filessend = ftp_nlist($conn_id, 'toRyder_arch');
+        }else{
+            Log::warning('Error en conexion ftp');
+            $filesnew = '0';
+            $filesall = '0';
+            $filessend = '0';
+        }
+
+        $ships = DB::table('edidaimlers')->latest()->get();
+
+        return \view('daimler.index', compact('ships','filesnew','filesall','filessend'));
     }
     
     public function show($id)
@@ -22,11 +46,69 @@ class EdidaimlerController extends Controller
             return \view('daimler.alert'); //no continuar
         } else {
             if (empty($valida->response)) { //valor null
-                
-                $data = DB::connection(env('DB_DAIMLER'))->table("edi_daimler")->select('shipment_identification_number', 'origin','addres_origin','city_origin','state_origin','country_origin','load_date_1','load_time_1','load_time_code_1', 'stop1','addres_stop1','city_stop1','state_stop1','country_stop1')->where('shipment_identification_number', '=', $id)->first();
+                $sql = DB::connection(env('DB_DAIMLER'))->table("edi_daimler_204")->where('shipment_identification_number', '=', $id)->first();
 
-                return \view('daimler.confirm', compact('data'));
+            //dd($sql);
 
+                    if($sql->stop8 ==! null){ //no es null
+                        $last = $sql->stop8;
+                        $addres_last = $sql->stop8_addres;
+                        $city_last = $sql->stop8_city;
+                        $state_last = $sql->stop8_state;
+                        $country_last = $sql->stop8_country;
+                    }elseif($sql->stop7 ==! null){
+                        $last = $sql->stop7;
+                        $addres_last = $sql->stop7_addres;
+                        $city_last = $sql->stop7_city;
+                        $state_last = $sql->stop7_state;
+                        $country_last = $sql->stop7_country;
+                    }elseif($sql->stop6 ==! null){
+                        $last = $sql->stop6;
+                        $addres_last = $sql->stop6_addres;
+                        $city_last = $sql->stop6_city;
+                        $state_last = $sql->stop6_state;
+                        $country_last = $sql->stop6_country;
+                    }elseif($sql->stop5 ==! null){
+                        $last = $sql->stop5;
+                        $addres_last = $sql->stop5_addres;
+                        $city_last = $sql->stop5_city;
+                        $state_last = $sql->stop5_state;
+                        $country_last = $sql->stop5_country;
+                    }elseif($sql->stop4 ==! null){
+                        $last = $sql->stop4;
+                        $addres_last = $sql->stop4_addres;
+                        $city_last = $sql->stop4_city;
+                        $state_last = $sql->stop4_state;
+                        $country_last = $sql->stop4_country;
+                    }elseif($sql->stop3 ==! null){
+                        $last = $sql->stop3;
+                        $addres_last = $sql->stop3_addres;
+                        $city_last = $sql->stop3_city;
+                        $state_last = $sql->stop3_state;
+                        $country_last = $sql->stop3_country;
+                    }elseif($sql->stop2 ==! null){
+                        $last = $sql->stop2;
+                        $addres_last = $sql->stop2_addres;
+                        $city_last = $sql->stop2_city;
+                        $state_last = $sql->stop2_state;
+                        $country_last = $sql->stop2_country;
+                    }
+                    $datos = array(
+                        'shipment_identification_number' => $sql->shipment_identification_number,
+                        'origin' => $sql->stop1,
+                        'addres_origin' => $sql->stop1_addres,
+                        'city_origin' => $sql->stop1_city,
+                        'state_origin' => $sql->stop1_state,
+                        'country_origin' => $sql->stop1_country,
+                        'load_date' => $sql->stop1_date,
+                        'load_time' => $sql->stop1_time,
+                        'last_destin' => $last,
+                        'addres_last' => $addres_last,
+                        'city_last' => $city_last,
+                        'state_last' => $state_last,
+                        'country_last' => $country_last);               
+                    $data = array_values($datos);
+                    return \view('daimler.confirm', compact('data' ,'sql'));
             } else {
                 return \view('daimler.alert'); //no continuar
             }
@@ -39,91 +121,57 @@ class EdidaimlerController extends Controller
         if (empty($Request->all())) {
             return \view('daimler.alert'); //no continuar
         } else {
-        if ($Request->response == 'A' || $Request->response == 'D') {
-        //almacena la respuesta en DB local del archivo recibido
-            DB::table('edidaimlers')->where('shipment_id', $Request->orderid)->update(['response' => $Request->response, 'updated_at' => $today->format('Y-m-d H:i:s') ]);
-        //consultar datos por id de la orden se envio
-            $datos = DB::connection(env('DB_DAIMLER'))->table("edi_daimler")->select('shipment_identification_number','purpose_code','Alpha_code','reference_identification_qualifier','reference_identification','load_date_qualifier_2','load_date_2','load_time_qualifier_2','load_time_2','load_time_code_2','id_qualifier_sender','id_sender','id_qualifier_receiver',
-            'id_receiver','version_number','control_number','sender_code','agency_code',
-            'industry_identifier','weight_units_load','weight_load','quantity_load','tracking_number_origin','id_tracking_number','tracking_number_stop1')->where('shipment_identification_number', '=', $Request->orderid)->first();
-        //Almacenamos la respuesta en tabla 990
-            DB::connection(env('DB_DAIMLER'))->table("edi_daimler_990")->insert([  
-                'shipment_identification_number' => $datos->shipment_identification_number,
-                'Alpha_code' => $datos->Alpha_code,
-                'reservation_action_code' => $Request->response,
-                'reference_identification_qualifier' => $datos->reference_identification_qualifier,
-                'reference_identification' => $datos->reference_identification,
-                'weight_units_load' => $datos->weight_units_load,
-                'weight_load' => $datos->weight_load,
-                'quantity_load' => $datos->quantity_load,
-                'load_date_qualifier_2' => $datos->load_date_qualifier_2,
-                'load_date_2' => $datos->load_date_2,
-                'load_time_qualifier_2' => $datos->load_time_qualifier_2,
-                'load_time_2' => $datos->load_time_2,
-                'load_time_code_2' => $datos->load_time_code_2,
-                'id_qualifier_sender' => $datos->id_qualifier_sender,
-                'id_sender' => $datos->id_sender,
-                'id_qualifier_receiver' => $datos->id_qualifier_receiver,
-                'id_receiver' => $datos->id_receiver,
-                'version_number' => $datos->version_number,
-                'control_number' => $datos->control_number,
-                'sender_code' => $datos->sender_code,
-                'agency_code' => $datos->agency_code,
-                'industry_identifier' => $datos->industry_identifier,
-                'date_time' => $today->format('Ymd H:i:s.000'),
-                'volume_unit_qualifier_load' => '',
-                'volume_load' => '',
-                'tracking_number_origin' => $datos->tracking_number_origin,
-                'id_tracking_number' => $datos->id_tracking_number,
-                'purpose_code' => $datos->purpose_code,
-                'tracking_number_stop1' => $datos->tracking_number_stop1
-            ]);
-        //consultamos la tabla 990 para confirmar el id y datos almacenado
-        $data990 = DB::connection(env('DB_DAIMLER'))->table("edi_daimler_990")->where('shipment_identification_number', '=', $Request->orderid)->first();
-        //Crear archivo TxT 990
-        $id = $data990->id_incremental;
-        $i = strlen($id);
-        if     ($i == 1) { $idnew = '00000000'.$id; }
-        elseif ($i == 2) { $idnew = '0000000'.$id; }
-        elseif ($i == 3) { $idnew = '000000'.$id; }
-        elseif ($i == 4) { $idnew = '00000'.$id; }
-        elseif ($i == 5) { $idnew = '0000'.$id; }
-        elseif ($i == 6) { $idnew = '000'.$id; } 
-        elseif ($i == 7) { $idnew = '00'.$id; }
-        elseif ($i == 8) { $idnew = '0'.$id; }
-        elseif ($i == 9) { $idnew = $id; } 
-        else { $idnew = 'null'; }
-        $tr0td5= $data990->id_qualifier_receiver;
-        $tr0td6= $data990->id_receiver;
-        $tr0td7= $data990->id_qualifier_sender;
-        $tr0td8= $data990->id_sender;
-        $tr0td9= date('ymd', strtotime($data990->date_time));
-        $tr0td10= date('Hi', strtotime($data990->date_time));
-        $tr0td11= $data990->version_number;
-        $tr0td12= $data990->control_number;    
-        $tr1td2= trim($data990->id_receiver);//retira espacio en blanco
-        $tr1td3= $data990->sender_code;
-        $tr1td4= date('Ymd', strtotime($data990->date_time));
-        $tr1td5= date('Hi', strtotime($data990->date_time));
-        $id= $data990->id_incremental;
-        $tr1td7= $data990->agency_code;
-        $tr1td8= $data990->industry_identifier;    
-        $tr3td1= $data990->alpha_code;
-        $tr3td2= $data990->shipment_identification_number;
-        $tr3td4= $data990->reservation_action_code;    
-        $tr4td1= $data990->reference_identification_qualifier;
-        $tr4td2= $data990->reference_identification;    
-        $tr5td1= $data990->load_date_qualifier_2;
-        $tr5td2= $data990->load_date_2;
-        $tr5td3= $data990->load_time_qualifier_2;
-        $tr5td4= $data990->load_time_2;
-        $tr5td5= $data990->load_time_code_2;
-        $filename = $data990->alpha_code.'_'.$data990->sender_code.'_990_'.$data990->load_date_2.'_'.$idnew;
-        //Crear archivo TxT 990
-        Storage::disk('ftp')->put('toRyder/'.$filename.'.txt', "ISA*00*          *00*          *".$tr0td5."*".$tr0td6."*".$tr0td7."*".$tr0td8."*".$tr0td9."*".$tr0td10."*".$tr0td11."*".$tr0td12."*".$idnew."*0*T*^~GS*GF*".$tr1td2."*".$tr1td3."*".$tr1td4."*".$tr1td5."*".$id."*".$tr1td7."*".$tr1td8."~ST*990*0001~B1*".$tr3td1."*".$tr3td2."**".$tr3td4."~N9*".$tr4td1."*".$tr4td2."~G62*".$tr5td1."*".$tr5td2."*".$tr5td3."*".$tr5td4."*".$tr5td5."~SE*5*0001~GE*1*".$id."~IEA*1*".$idnew."~");
-        } else {
-            return response()->json(['message'=>'Respuesta incorrecta'], 200);
-        }
+            if ($Request->response == 'A' || $Request->response == 'D') {
+            //almacena la respuesta en DB local del archivo recibido
+                $mysql = DB::table('edidaimlers')->where('shipment_id', $Request->orderid)->update(['response' => $Request->response, 'updated_at' => $today->format('Y-m-d H:i:s') ]);
+                    if(empty($mysql)){
+                        Log::warning('Error en almacenar (mysql) respuesta de orden: '.$Request->orderid);
+                    }
+            //consultar datos por id de la orden se envio
+                $datos = DB::connection(env('DB_DAIMLER'))->table("edi_daimler_204")->where('shipment_identification_number', '=', $Request->orderid)->first();
+            //Almacenamos la respuesta en tabla 990
+                $update990 = DB::connection(env('DB_DAIMLER'))->table("edi_daimler_990")->insert([  
+                    'shipment_identification_number' => $datos->shipment_identification_number,
+                    'alpha_code' => $datos->alpha_code,
+                    'reservation_action_code' => $Request->response,
+                    'reference_identification_qualifier' => $datos->reference_identification_qualifier,
+                    'reference_identification' => $datos->reference_identification,
+                    // no se guardaron
+                        //'weight_units_load' => $datos->weight_units_load,
+                        //'weight_load' => $datos->weight_load,
+                        //'quantity_load' => $datos->quantity_load,
+                    //esta en G62 solo guardamos fecha, hora y code
+                        // 'load_date_qualifier_2' => $datos->load_date_qualifier_2,
+                        // 'load_date_2' => $datos->load_date_2,
+                        // 'load_time_qualifier_2' => $datos->load_time_qualifier_2,
+                        // 'load_time_2' => $datos->load_time_2,
+                        // 'load_time_code_2' => $datos->load_time_code_2,
+                    'id_qualifier_sender' => $datos->id_qualifier_sender,
+                    'id_sender' => $datos->id_sender,
+                    'id_qualifier_receiver' => $datos->id_qualifier_receiver,
+                    'id_receiver' => $datos->id_receiver,
+                    'version_number' => $datos->version_number,
+                    'control_number' => $datos->control_number,
+                    'sender_code' => $datos->sender_code,
+                    'agency_code' => $datos->agency_code,
+                    'industry_identifier' => $datos->industry_identifier,
+                    'date_time' => $today->format('Ymd H:i:s.000'),
+                // no se guardaron 
+                    // 'volume_unit_qualifier_load' => '',
+                    // 'volume_load' => '',
+                    'tracking_number_origin' => $datos->stop1_tracking_number,
+                    // esta en L11[2] solo se guarda el L11[1]
+                    //'id_tracking_number' => $datos->id_tracking_number,
+                    'purpose_code' => $datos->purpose_code,
+                    'tracking_number_stop1' => $datos->stop2_tracking_number //varia los stop debera ser el ultimo se deja el stop2
+                ]);
+                if (empty($update990)) { Log::critical('Fallo al actualizar tabla: edi_daimler_990'); }
+                else {
+                    Log::info('tabla: edi_daimler_990 actualizada con exito');
+                }
+            } else {
+                return response()->json(['message'=>'Respuesta incorrecta'], 200);
+            }
             return response()->json(['message'=>'successful response'], 200);
         }
     }
