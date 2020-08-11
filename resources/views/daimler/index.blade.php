8adcbfb513dbd3b4603a1d448cd27a2363ebb831
manuelvidales <virmanuel@gmail.com>
2020-08-11 17:04:35 -0500

    agrega link filename

--- resources/views/daimler/index.blade.php
+++ resources/views/daimler/index.blade.php
@@ -1,9 +1,76 @@
 @extends('layouts.app')
 @section('content')
-<div class="jumbotron">
+
+<div class="jumbotron p-2">
+
+<div class="card-deck p-4">
+  <div class="card text-white text-center bg-dark shadow rounded">
+    <div class="row justify-content-md-center">
+      <div class="col-3"><i class="fas fa-inbox fa-5x"></i></div>
+      <div class="col-3"><h1 class="font-weight-bold"><span class="badge badge-primary">
+        @php
+             echo count($ships);
+        @endphp
+      </span></h1></div>
+    </div>
+    <div class="card-body">
+      <h5 class="card-title"><button type="button" class="btn btn-outline-light">Documentos totales</button></h5>
+      <!-- <p class="card-text"><small class="">actualizado hace 3 minutos</small></p> -->
+    </div>
+  </div>
+
+  <div class="card text-white text-center bg-dark">
+    <div class="row justify-content-md-center">
+      <div class="col-2"><i class="fas fa-file-download fa-5x"></i></div>
+      <div class="col-2"><h1 class="font-weight-bold"><span class="badge badge-primary">
+        @php
+        echo count($filesnew);
+        @endphp
+      </span></h2></div>
+    </div>
+    <div class="card-body">
+      <h5 class="card-title"><button type="button" class="btn btn-outline-light">Recepcion archivos</button></h5>
+      <!-- <p class="card-text"><small class="">actualizado hace 3 minutos</small></p> -->
+    </div>
+  </div>
+
+  <div class="card text-white text-center bg-dark">
+    <div class="row justify-content-md-center">
+      <div class="col-2"><i class="fas fa-file-upload fa-5x"></i></div>
+      <div class="col-2"><h1 class="font-weight-bold"><span class="badge badge-primary">
+        @php
+        echo count($filessend);
+        @endphp  
+      </span></h2></div>
+    </div>
+    <div class="card-body">
+      <h5 class="card-title"><button type="button" class="btn btn-outline-light">Recepcion confirmadas</button></h5>
+      <!-- <p class="card-text"><small class="">actualizado hace 3 minutos</small></p> -->
+    </div>
+  </div>
+
+  <div class="card text-white text-center bg-dark">
+    <div class="row justify-content-md-center">
+      <div class="col-2"><i class="fas fa-file-alt fa-5x"></i></div>
+      <div class="col-2"><h1 class="font-weight-bold"><span class="badge badge-primary">X
+        
+      </span></h2></div>
+    </div>
+    <div class="card-body">
+      <h5 class="card-title"><button type="button" class="btn btn-outline-light">Ordenes confirmadas</button> </h5>
+      <!-- <p class="card-text"><small class="">actualizado hace 3 minutos</small></p> -->
+    </div>
+  </div>
+  
+</div>
+</div>
+
+
+
+<div class="jumbotron p-4">
     <div class="card shadow p-3 mb-5 bg-white rounded">
         <div class="card-header text-center">
-        <h4>  Ordenes recibidas de Daimler</h4>
+        
         </div>
         <div class="card-body">
         <div class="row justify-content-md-center">
@@ -12,8 +79,9 @@
             <table class="table table-sm table-striped table-hover" id="pedidos">
               <thead class="thead-light">
                   <tr>
+                      <th></th>
                       <th>archivo</th>
-                      <th>Id envio</th>
+                      <th>Id orden</th>
                       <th>Respuesta</th>
                       <th>Fecha recepcion</th>
                   </tr>
@@ -21,8 +89,9 @@
               <tbody>
                   @foreach ($ships as $data)  
                   <tr>
+                    <td><a href="{{$data->filename}}" ><i class="fas fa-download"></i></a></td>
                     <td>
-                      <?php $files = "$data->filename"; echo substr($files, 10); ?> 
+                      <?php $files = "$data->filename"; echo substr($files, 10); ?>
                     </td>
                     <td>{{$data->shipment_id}}</td>
                     @if ( $data->response == 'A')
@@ -47,6 +116,7 @@
 <script type="text/javascript">
   $(document).ready(function() {
   $('#pedidos').DataTable({
+    ordering: false,
       "language": {
           "sProcessing":     "Procesando...",
               "sLengthMenu":     "Mostrar _MENU_ registros",
