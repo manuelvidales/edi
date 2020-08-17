d576d097a4d2e6b7e725311cba9543060d4ef4a8
manuelvidales <virmanuel@gmail.com>
2020-08-17 11:06:16 -0500

    agrega carpeta de logs

--- config/filesystems.php
+++ config/filesystems.php
@@ -55,6 +55,11 @@ return [
             'visibility' => 'public',
         ],
 
+        'log' => [
+            'driver' => 'local',
+            'root' => storage_path('logs'),
+        ],
+
         's3' => [
             'driver' => 's3',
             'key' => env('AWS_ACCESS_KEY_ID'),
