8ad6cf5a5cbc0a07d75c701e244480ca7ebd787d
manuelvidales <virmanuel@gmail.com>
2020-09-01 11:45:39 -0500

    agrega 204 & 824 con datatable

--- public/js/filestxt.js
+++ public/js/filestxt.js
@@ -7,7 +7,7 @@ $.ajaxSetup({
         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
     }
 });
-//mostrar archivos
+//leer texto archivos
 $(document).on('click', '.viewfiles', function(){
     let id = $(this).closest('tr').data('id');
     let name = $(this).closest('tr').data('name');
@@ -46,7 +46,7 @@ $(document).on('click', '.viewfiles', function(){
           }
       });
   });
-  
+//update header contadores
 $(document).ready(function () {
   let store = $('#almacen');
   let recent = $('#nuevos');
@@ -65,4 +65,107 @@ $(document).ready(function () {
         $(warning).append(``+data[9]+``);
       });
   }, 60000);//(1min)
+});
+//recargar pagina al seleccionar 204
+$(document).on('click', '.code204', function(){
+  location.reload();
+});
+// tabla code 824
+$(document).on('click', '.code824', function(){
+  let tabla = $('#tablaEdicode');
+  $(tabla).html('');
+  $(tabla).append(`
+  <div class="card shadow p-3 mb-5 bg-white rounded">
+    <div class="card-body">
+      <div class="row justify-content-md-center">
+        <div class="table-responsive">
+          <table class="table table-sm table-striped table-hover" id="code824">
+            <thead class="thead-light">
+              <tr>
+                <th>Archivo</th>
+                <th>Tender</th>
+                <th>Tipo</th>
+                <th>Estatus</th>
+                <th>Fecha recepcion</th>
+              </tr>
+            </thead>
+            <tbody id="mostrarfiles824">
+
+            </tbody>
+          </table> 
+        </div>
+      </div>
+    </div>
+  </div>
+  `);
+    $.ajax({
+      type:'GET',
+      url:'/files824',
+      success: function(data){
+        for (i = 0; i < data.length; i++){
+            let dt = new Date(data[i].created_at);
+            //dar fomato fecha dd/mm/yyy HH:mm
+            fecha = (`${dt.getDate().toString().padStart(2, '0')}/
+                ${(dt.getMonth()+1).toString().padStart(2, '0')}/${
+                dt.getFullYear().toString().padStart(4, '0')} ${
+                dt.getHours().toString().padStart(2, '0')}:${
+                dt.getMinutes().toString().padStart(2, '0')}`
+                );
+            if (data[i].status == 0) {
+              $('#mostrarfiles824').append(`
+              <tr data-id="`+data[i].id+`" data-name="`+data[i].filename+`">
+                <td> <a href="/files/`+data[i].id+`"><i class="fas fa-download"></i></a> <a href="#" class="viewfiles" data-toggle="modal" data-target="#verarchivo"><i class="fas fa-eye"></i></a>
+                </td>
+                <td>`+data[i].shipment_id+`</td>
+                <td>`+data[i].code+`</td>
+                <td>recepcion</td>
+                <td>`+fecha+`</td>
+              </tr>
+              `);
+            } else {
+            $('#mostrarfiles824').append(`
+              <tr data-id="`+data[i].id+`">
+                <td> <a href="/files/`+data[i].id+`"><i class="fas fa-download"></i></a> <a href="#" class="viewfiles" data-toggle="modal" data-target="#verarchivo"><i class="fas fa-eye"></i></a>
+                </td>
+                <td>`+data[i].shipment_id+`</td>
+                <td>`+data[i].code+`</td>
+                <td>procesado</td>
+                <td>`+fecha+`</td>
+                </tr>
+              `);
+            }
+        }
+        $('#code824').DataTable({
+          ordering: false,
+            "language": {
+                "sProcessing":     "Procesando...",
+                    "sLengthMenu":     "Mostrar _MENU_ registros",
+                    "sZeroRecords":    "No se encontraron coincicendias",
+                    "sEmptyTable":     "Ningún dato disponible en esta consulta",
+                    "sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                    "sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+                    "sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+                    "sInfoPostFix":    "",
+                    "sSearch":         "Buscar:",
+                    "sUrl":            "",
+                    "sInfoThousands":  ",",
+                    "sLoadingRecords": "Cargando...",
+                    "oPaginate": {
+                        "sFirst":    "Primero",
+                        "sLast":     "Último",
+                        "sNext":     "Siguiente",
+                        "sPrevious": "Anterior"
+                    },
+                    "oAria": {
+                        "sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+                        "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                    },
+                    "buttons": {
+                        "copy": "Copiar",
+                        "colvis": "Visibilidad"
+                    }
+                }
+        });
+      }
+    });
 });
