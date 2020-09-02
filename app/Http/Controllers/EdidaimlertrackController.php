47e925ecf8c8e1ce827773b52e5c7c3c9770c998
manuelvidales <virmanuel@gmail.com>
2020-09-02 12:36:50 -0500

    nuevo del modelo edidaimlertrack

---
+++ app/Http/Controllers/EdidaimlertrackController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\edidaimlertrack;
+use Illuminate\Http\Request;
+
+class EdidaimlertrackController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\edidaimlertrack  $edidaimlertrack
+     * @return \Illuminate\Http\Response
+     */
+    public function show(edidaimlertrack $edidaimlertrack)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\edidaimlertrack  $edidaimlertrack
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(edidaimlertrack $edidaimlertrack)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\edidaimlertrack  $edidaimlertrack
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, edidaimlertrack $edidaimlertrack)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\edidaimlertrack  $edidaimlertrack
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(edidaimlertrack $edidaimlertrack)
+    {
+        //
+    }
+}
