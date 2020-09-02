536c1d9bf2756d81a89f849257d9702676ec42cc
manuelvidales <virmanuel@gmail.com>
2020-09-02 12:37:00 -0500

    nuevo del modelo edidaimlertrack

---
+++ database/factories/EdidaimlertrackFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\edidaimlertrack;
+use Faker\Generator as Faker;
+
+$factory->define(edidaimlertrack::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
