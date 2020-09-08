f60ef1d1ea7827cb4848d1c9bf66d7d783793e05
manuelvidales <virmanuel@gmail.com>
2020-09-08 15:54:11 -0500

    agrega librerias mapbox

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -31,6 +31,9 @@
             <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"
             integrity="sha512-XQoYMqMTK8LvdxXYG3nZ448hOEQiglfqkJs1NOQV44cWnUrBc8PkAOcXy20w0vlaXaVUearIOBhiXZ5V3ynxwA=="
             crossorigin=""></script>
+        <!-- JS Mapbox -->
+            <script src='https://api.mapbox.com/mapbox.js/v3.3.1/mapbox.js'></script>
+            <link href='https://api.mapbox.com/mapbox.js/v3.3.1/mapbox.css' rel='stylesheet' />
 </head>
 <body>
     <!-- Loading screen -->
