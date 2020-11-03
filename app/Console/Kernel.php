722949dcd603382b6d908d1c096fc428d599bcdf
manuelvidales <virmanuel@gmail.com>
2020-11-03 12:25:44 -0600

    se comenta task temporal

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -37,8 +37,8 @@ class Kernel extends ConsoleKernel
         $schedule->command('edi214gps:daimler')->hourly();
         $schedule->command('edi210:visteon')->everyMinute();
         $schedule->command('log:laravel')->daily();
-        $schedule->command('Notificaciones:clientes')->everyMinute();
-        $schedule->command('Notificaciones:clientesGps')->everyThirtyMinutes();
+        // $schedule->command('Notificaciones:clientes')->everyMinute();
+        // $schedule->command('Notificaciones:clientesGps')->everyThirtyMinutes();
     }
 
     /**
